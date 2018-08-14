eefa262b6e5e59f523b0db803811781c3bbf3dca
Sumit Maharjan <timusumit@gmail.com>
2018-08-14 19:54:21 +0545

    social

--- application/views/admin/social.php
+++ application/views/admin/social.php
@@ -6,10 +6,30 @@
                         <div class="card">
                             <div class="header">
                                 <h4 class="title">Social Links Setup</h4>
-                                <p class="category">Created using Roboto Font Family</p>
+                                <p class="category">Social Media is best way to boost SEO Links to your favourite social media can be setup here.</p>
                             </div>
                             <div class="content">
-
+                            	<div class="row">
+                            	<div class="col-lg-6">
+                            	<div class="input-group">
+                            		<span class="input-group-addon bg-info">
+                            			Select Social Media Site:
+                            		</span>
+                            		<select class="form-control">
+                            			<option>Facebook</option>
+                            			<option>Twitter</option>
+                            			<option>Youtube</option>
+                            			<option>Instagram</option>
+                            			<option>LinkedIn</option>
+                            			<option>Google</option>
+                            		</select>
+                            	</div>
+                            	<div class="input-group mar-5-top">
+                            		<span class="input-group-addon bg-info">Username:</span>
+                            		<input type="text" class="form-control">
+                            	</div>
+                            	</div>
+                            	</div>
 
                             </div>
                         </div>
